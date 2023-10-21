01138fa4ba6db93f5a91619169e966448eb0433b
Juan David Sanguino <juandavid.sanguino@doublevpartners.com>
2023-10-21 18:04:58 -0500

    create dashboard

---
+++ src/assets/constants/index.js
@@ -0,0 +1,24 @@
+import Home from "@/assets/svg/home.svg";
+
+export const sidebarLinks = [
+  {
+    imgURL: Home,
+    route: "/dashboard",
+    label: "Dashboard",
+  },
+  {
+    imgURL: Home,
+    route: "/create-user",
+    label: "Crear Usuario",
+  },
+  {
+    imgURL: Home,
+    route: "/search",
+    label: "Buscar",
+  },
+  {
+    imgURL: Home,
+    route: "/profile",
+    label: "Perfil",
+  },
+];
---
+++ src/presentation/pages/Dashboard/View.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Dashboard = () => {
+  return <h1 className="text-3xl leading-{140%} font-bold">Home</h1>;
+};
+
+export default Dashboard;
