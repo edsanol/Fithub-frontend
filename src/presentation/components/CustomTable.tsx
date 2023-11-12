423c050ffd10975cefe67559a2c41ee304e64048
Juan David Sanguino <1092526224@medellin.gov.co>
2023-11-11 22:57:31 -0500

    Update tooltips in CustomTable component

--- src/presentation/components/CustomTable.tsx
+++ src/presentation/components/CustomTable.tsx
@@ -111,17 +111,17 @@ const CustomTable = ({
       case "actions":
         return (
           <div className="relative flex items-center gap-2">
-            <Tooltip content="Details" classNames={{ base: "dark" }}>
+            <Tooltip content="Ver detalle" classNames={{ base: "dark" }}>
               <span className="text-lg text-default-400 cursor-pointer active:opacity-50">
                 <EyeIcon />
               </span>
             </Tooltip>
-            <Tooltip content="Edit user" classNames={{ base: "dark" }}>
+            <Tooltip content="Editar usuario" classNames={{ base: "dark" }}>
               <span className="text-lg text-default-400 cursor-pointer active:opacity-50">
                 <EditIcon />
               </span>
             </Tooltip>
-            <Tooltip color="danger" content="Delete user">
+            <Tooltip color="danger" content="Eliminar usuario">
               <span className="text-lg text-danger cursor-pointer active:opacity-50">
                 <DeleteIcon />
               </span>
