10f0290eb45fe4388c46b73860771b24920c750c
Juan David Sanguino <juandavid.sanguino@doublevpartners.com>
2024-02-11 13:57:53 -0500

    feat: modify layout

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,26 +1,36 @@
+"use client";
+
 import { Bottombar, LeftSidebar, Topbar } from "@/presentation/components";
 import type { Metadata } from "next";
+import { useState } from "react";
 
-export const metadata: Metadata = {
-  title: "FitHub Connect - Dashboard Administrativo",
-  description: "Dashboard Administrativo para FitHub Connect",
-};
+// export const metadata: Metadata = {
+//   title: "FitHub Connect - Dashboard Administrativo",
+//   description: "Dashboard Administrativo para FitHub Connect",
+// };
 
 export default function DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
   return (
     <>
-      <Topbar />
-
       <main className="flex flex-row">
-        <LeftSidebar />
+        <LeftSidebar
+          sidebarOpen={sidebarOpen}
+          setSidebarOpen={setSidebarOpen}
+        />
 
-        <section className="flex min-h-screen flex-1 flex-col items-center px-6 pb-10 pt-28 max-md:pb-32 sm:px-10 overflow-auto bg-black">
-          <div className="w-full max-w-4xl md:max-w-7xl">{children}</div>
-        </section>
+        <div className="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
+          <Topbar />
+
+          <section className="flex min-h-screen flex-1 flex-col items-center px-6 pb-10 pt-28 max-md:pb-32 sm:px-10 overflow-auto bg-black">
+            <div className="w-full max-w-4xl md:max-w-7xl">{children}</div>
+          </section>
+        </div>
       </main>
 
       <Bottombar />
