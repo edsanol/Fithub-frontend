1ccd2283b261e679aa1f8f68cda013db278be5b7
Juan David Sanguino <juandavidsanguinoo@gmail.com>
2025-01-30 22:49:48 -0500

    Enhance layout and modal behavior for improved user experience

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -22,7 +22,7 @@ export default function DashboardLayout({
             <TopbarLayout />
 
             <main>
-              <div className="mx-auto max-w-screen-2xl p-4 md:p-6">
+              <div className="mx-auto max-w-screen-2xl p-4 mt-16 md:p-6 md:mt-10">
                 {children}
               </div>
             </main>
